8c5c3de2831a9df897cc8116ff37c5fef083b9fc
louisclarencepeter <louisclarencepeters@gmail.com>
2023-06-27 15:52:57 +0200

    react

---
+++ live-coding-frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import ProfileCard from "./components/ProfileCard.jsx";
+function App() {
+  return (
+    <>
+      <ProfileCard />
+    </>
+  );
+}
+
+export default App;
