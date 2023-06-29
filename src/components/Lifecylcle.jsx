e046b3ace4ab4972b50b361eb50a88002fc639e6
louisclarencepeter <louisclarencepeters@gmail.com>
2023-06-29 09:53:31 +0200

    components

--- src/components/Lifecylcle.jsx
+++ src/components/Lifecylcle.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 const Lifecylcle = () => {
-  return <div>Lifecylcle</div>;
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log("componentDidMount");
+  }, []);
+
+  return (
+    <div className="m-3">
+      <div >
+        <button className="btn btn-primary" onClick={() => setCount(count + 1)}>Click me</button>
+      </div>
+
+      <div className="text-secondary display-5">{count}</div>
+    </div>
+  );
 };
 
 export default Lifecylcle;
