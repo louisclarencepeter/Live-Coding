5bb5689f27d34f40008ceccb2059239f4651f1fb
louisclarencepeter <louisclarencepeters@gmail.com>
2023-06-30 08:57:13 +0200

    done for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import Content from "./components/Content";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
   return (
-    <>
+    <div>
+      <Header />
       <Content />
-    </>
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div>
+        <h1>I am Header section</h1>
+        <img src="src/components/images/IMG_8327.JPG" alt="random" />
+    </div>
+  )
+}
+
+export default Header
