534f899277be4a2399c077a4efead16c2b3dc3f5
louisclarencepeter <louisclarencepeters@gmail.com>
2023-06-30 08:23:33 +0200

    live from yesterday

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,38 +1,34 @@
-import React from "react";
-import { useEffect } from "react";
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import Spinner from "./Spinner";
 
 function Posts() {
-  const [posts, setPosts] = useState([]);
-  const [loading, setLoading] = useState(true);
+    const [posts, setPosts] = useState([]);
+    const [isLoading, setIsloading] = useState(true);
 
-  useEffect(() => {
-    setTimeout(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((response) => response.json())
-      .then((data) => setPosts(data))
-      .catch((error) => console.log(error));
-      .finally(() => setLoading(!loading)));
-  }, 4000); },
-  []);
+    useEffect(() => {
+        setTimeout(() => {
+            fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(res => res.json())
+            .then(fetchedPosts => setPosts(fetchedPosts))
+            .catch(error => console.log(error))
+            .finally(() => setIsloading(!isLoading));
+        }, 4000);
+    }, [])
 
-  if (loading) return <Spinner />;
+    if (isLoading) return <Spinner />
 
-  //console.log(posts);
-
-  return (
-    <div className="container mt-5">
-      <ul className="list-group">
-        {posts.map(post => 
-          <li 
-          className="list-group-item" 
-          key={post.id}>{post.title}
-          </li>
-        )}
-      </ul>
-    </div>
-  );
+    return (
+        <div className="container mt-5">
+            <ul className="list-group">
+                {posts.map(post => 
+                    <li 
+                        className="list-group-item" 
+                        key={post.id}>{post.title}
+                    </li>
+                )}
+            </ul>
+        </div>
+    )
 }
 
-export default Posts;
+export default Posts
