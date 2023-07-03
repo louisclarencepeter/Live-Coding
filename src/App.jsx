0d095e07a2b79df23633c072953dc63e7891d035
louisclarencepeter <louisclarencepeters@gmail.com>
2023-07-03 09:28:43 +0200

    Created Components and Formated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/About";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <>
+      <About />
+      <Contact />
     </>
   );
 }
---
+++ src/components/Contact.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus eum
+        vitae libero perferendis sit, rerum incidunt quas sunt nostrum
+        cupiditate quis iste eaque modi voluptate deleniti. Autem quibusdam ipsa
+        quam.
+      </p>
+    </div>
+  );
+}
+
+export default Contact;
