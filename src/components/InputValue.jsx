3f53e1f61d7ff07a2d3f80901210eaac820dd1ca
louisclarencepeter <louisclarencepeters@gmail.com>
2023-07-06 08:51:53 +0200

    created the input component

---
+++ src/components/InputValue.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react';
+
+function InputValue() {
+
+    const [userName, setUserName] = useState("");
+
+    const handleUserNameChange = (event) => {
+        setUserName(event.target.value);
+    };
+
+    return (
+        <div className='form-group m-3'>
+            <form>
+                <label className='form-lable'>
+                    UserName
+                </label>
+                <input type="text" onChange={handleUserNameChange} value={userName} />
+            </form>
+
+        </div>
+    );
+}
+
+export default InputValue
